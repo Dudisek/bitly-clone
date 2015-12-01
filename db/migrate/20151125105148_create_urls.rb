044cf0a088272e7274a7229c12e63b6a6e4067a0
Dudisek <peta.duda@gmail.com>
2015-12-01 15:37:22 +0800

    Imported txt file with 500k URLS - very slow

--- db/migrate/20151125105148_create_urls.rb
+++ db/migrate/20151125105148_create_urls.rb
@@ -8,3 +8,8 @@ class CreateUrls < ActiveRecord::Migration
 		end
 	end
 end
+
+
+	# INSERT INTO films (code, title, did, date_prod, kind) VALUES
+ #    ('B6717', 'Tampopo', 110, '1985-02-10', 'Comedy'),
+ #    ('HG120', 'The Dinner Game', 140, DEFAULT, 'Comedy');
